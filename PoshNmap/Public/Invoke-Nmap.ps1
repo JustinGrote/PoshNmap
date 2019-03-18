6051b11e764a76af2b9c057f8a722c7ae3c835c9
JustinGrote <justingrote+github@gmail.com>
2019-03-18 14:27:34 -0700

    Test: Add Verbose message about what NMAP command is being executed

--- PoshNmap/Public/Invoke-Nmap.ps1
+++ PoshNmap/Public/Invoke-Nmap.ps1
@@ -1,20 +1,25 @@
-#Register-ArgumentCompleter -CommandName "Invoke-Nmap" -ParameterName "Preset" -ScriptBlock {[PoshNmapPresetArguments].GetEnumNames()}
+Register-ArgumentCompleter -CommandName "Invoke-Nmap" -ParameterName "Preset" -ScriptBlock {(Get-NmapPresetArguments).keys}
+
 function Invoke-Nmap {
     <#
     .SYNOPSIS
         Runs the NMAP command and then formats the output.
-    .EXAMPLE
-        Invoke-Nmap www.google.com
-        Runs an NMAP scan of www.google.com and provides the results as powershell objects
     .INPUTS
         [String[]]$ComputerName
     .OUTPUTS
-        [PoshNMAP.NMAPResult]
+        Depends on -OutFormat parameter
+    .EXAMPLE
+        Invoke-Nmap scanme.nmap.org
+        Runs an NMAP scan with the Quick scan preset and provides the result as a formatted Powershell Object
+    .EXAMPLE
+        Invoke-Nmap scanme.nmap.org "-t4 -p 80,443"
+        This is similar to running nmap "bare" but enjoy the format processing of invoke-nmap
+
     #>
-`
+
     [CmdletBinding(DefaultParameterSetName="preset")]
     param (
-        #A list of nmap host specifications
+        #A list of nmap host specifications. Defaults to localhost
         [Parameter(
             Position=0,
             ValueFromPipeline
@@ -22,33 +27,39 @@ function Invoke-Nmap {
         [String[]]
         $computerName = "localhost",
 
-        #Override the default nmap parameters
+        #Specify raw argument parameters to nmap
         [String[]]
         [Parameter(
-            Position=1,
-            ParameterSetName="custom"
+            ParameterSetName="custom",
+            Mandatory,
+            Position=1
         )]
-        $ArgumentList = (Get-NmapPresetArguments -Preset 'Quick'),
+        $ArgumentList,
 
-        [String[]]
+        [String]
         [Parameter(
             ParameterSetName="preset",
             ValueFromPipeline
         )]
-        $Preset = "Quick",
+        [String]$Preset = "Quick",
+
+        #Choose which format for the output (XML, JSON, HashTable, PSObject, or Raw). Default is PSObject
+        [ValidateSet('Raw','PSObject','XML','JSON','Hashtable')]
+        [String]$OutFormat = 'PSObject',
 
         #A list of SNMP communities to scan. Defaults to public and private
         [String[]]
         [Parameter()]
-        $snmpCommunityList = @("private","public"),
-
-        #Choose which format for the output (XML, JSON, HashTable, or PSObject). Default is PSObject
-        [ValidateSet('PSObject','XML','JSON','Hashtable')]
-        [String]$OutFormat = 'PSObject'
+        $snmpCommunityList = @("private","public")
     )
 
     if ($Preset) {
-        $ArgumentList = Get-NmapPresetArguments $Preset
+        $nmapPresetArgumentNames = (Get-NmapPresetArguments).keys
+        if ($Preset -notin $nmapPresetArgumentNames) {
+            throwUser New-Object ArgumentException -ArgumentList "Invoke-Nmap: Value $Preset is not a valid choice. Please choose one of: $($nmapPresetArgumentNames -join ', ')","Preset"
+        } else {
+            $ArgumentList = Get-NmapPresetArguments $Preset
+        }
     }
 
     if ($snmpCommunityList) {
@@ -58,13 +69,20 @@ function Invoke-Nmap {
     }
     $nmapexe = 'nmap.exe'
 
-    if (-not $Raw) {
-        $ArgumentList += "-oX","-"
+    if ($OutFormat -eq 'Raw') {
+        Invoke-NmapExe $nmapExe $argumentList $computerName
+        exit $LASTEXITCODE
     }
 
-    [String]$nmapresult = Invoke-NmapExe $nmapExe $argumentList $computerName
+    $ArgumentList += "-oX","-"
+    try {
+        [String]$nmapresult = Invoke-NmapExe $nmapExe $argumentList $computerName
+    } finally {
+        if (Test-Path $snmpCommunityFile) {Remove-Item $snmpCommunityFile -Force -ErrorAction SilentlyContinue}
+    }
 
-    if (-not $nmapResult) {throw "NMAP did not produce any output. Please review any errors that are present above this warning."}
+
+    if (-not $nmapResult) {throwUser "NMAP did not produce any output. Please review any errors that are present above this warning."}
     switch ($OutFormat) {
         'XML' {
             $nmapResult
@@ -79,6 +97,4 @@ function Invoke-Nmap {
             $nmapResult | ConvertFrom-NmapXML -OutFormat HashTable
         }
     }
-
-    Remove-Item $snmpCommunityFile -Force -ErrorAction SilentlyContinue
 }
