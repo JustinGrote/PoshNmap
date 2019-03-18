c84d0c188b15174d324a356292375011ba6446b4
JustinGrote <justingrote+github@gmail.com>
2019-03-18 14:25:57 -0700

    Rearrange Presets

--- PoshNmap/Private/Get-NmapPresetArguments.ps1
+++ PoshNmap/Private/Get-NmapPresetArguments.ps1
@@ -1,14 +1,15 @@
 #TODO: Pull this from a configuration file
 function Get-NmapPresetArguments ($Preset) {
-    $presetList = @{
+    $presetList = [ordered]@{
         Default = ''
         Intense = '-T4 -A' -split '\s'
-        IntensePlusUDP = '-sS -sU -T4 -A' -split '\s'
-        IntenseAllTCP = '-p 1-65535 -T4 -A' -split '\s'
+        IntenseAllTCP = '-T4 -p 1-65535 -A' -split '\s'
+        IntensePlusUDP = '-T4 -sS -sU -A' -split '\s'
         IntenseNoPing = '-T4 -A -v -Pn' -split '\s'
+        PingSweep = '-T4 -sn'
         Quick = '-T4 -F' -split '\s'
-        QuickPlus = '-sV -T4 -O -F –version-light' -split '\s'
-        QuickTraceroute = '-sn -traceroute' -split '\s'
+        QuickPlus = '-T4 -sV -O -F –version-light' -split '\s'
+        QuickTraceroute = '-T4 -sn -traceroute' -split '\s'
     }
 
     if ($Preset) {
