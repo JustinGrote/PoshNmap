bc2b752f75cbf185a2b686b408da639273d8a256
JustinGrote <justingrote+github@gmail.com>
2019-03-18 14:26:34 -0700

    Refactor: Output Forms

--- PoshNmap/Public/ConvertFrom-NmapXML.ps1
+++ PoshNmap/Public/ConvertFrom-NmapXML.ps1
@@ -1,48 +1,66 @@
 using namespace newtonsoft.json
-
+using namespace system.management.automation
 function ConvertFrom-NmapXml {
 <#
 .SYNOPSIS
 Converts NmapXML into various formats. Currently supported are JSON, PSObject, NmapReport
 .EXAMPLE
-nmap localhost -oX - | ConvertFrom-NmapXML
+nmap localhost -oX - | ConvertFrom-NmapXML -OutFormat JSON
+Takes an NMAP run output and converts it into JSON
+
 #>
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName="String")]
     param (
         #The NMAPXML content
-        [Parameter(ValueFromPipeline)][XML[]]$InputObject,
+        [Parameter(ParameterSetName='String',ValueFromPipeline)][String[]]$InputString,
+        [Parameter(ParameterSetName='XML',ValueFromPipeline)][XML[]]$InputObject,
 
         #Choose the format that you want to convert the NMAPXML to. Valid options are: JSON or HashTable
         [ValidateSet('JSON','HashTable','PSObject')]
         $OutFormat = 'JSON'
     )
 
-    $jsonResult = foreach ($nmapXmlItem in $InputObject) {
-
-        #Selecting NmapRun required for PS5.1 compatibility due to Newtonsoft.Json bug
-        $nmapRunItem = ($nmapXmlItem).SelectSingleNode('nmaprun')
-
-        #Indented JSON is important as we will use a regex to clean up the @ elements
-        $convertedJson = [JsonConvert]::SerializeXmlNode($nmapRunItem,'Indented')
-
-        #Remove @ symbols from xml attributes. There are no element/attribute collisions in the nmap xml (that we know of) so this should be OK.
-        [Regex]$MatchConvertedAmpersand = '(?m)(?<=\s+\")(@)(?=.+\"\:)'
-        $convertedJson = $convertedJson -replace $MatchConvertedAmpersand,''
-        $convertedJson
+    process {
+        #If strings were passed via pipeline, assume it is output from nmap XML which is multiple lines and coalesce them into one large document.
+        $InputObjectBundle += $InputString
     }
 
-    switch ($OutFormat) {
-        'JSON' {
-            return $jsonResult
+    end {
+        try {
+            [XML]$CombinedDocument = $InputObjectBundle
+        } catch [InvalidCastException] {
+            $exception = [System.Management.Automation.PSInvalidCastException]::New("The input provided is not valid XML. If you are piping from nmap, did you use 'nmap -oX -'?")
+            throwUser $exception
         }
-        'PSObject' {
-            return $jsonResult | ConvertFrom-Json
+
+        if ($CombinedDocument) {$inputObject = $CombinedDocument}
+        $jsonResult = foreach ($nmapXmlItem in $InputObject) {
+
+            #Selecting NmapRun required for PS5.1 compatibility due to Newtonsoft.Json bug
+            $nmapRunItem = ($nmapXmlItem).SelectSingleNode('nmaprun')
+
+            #Indented JSON is important as we will use a regex to clean up the @ elements
+            $convertedJson = [JsonConvert]::SerializeXmlNode($nmapRunItem,'Indented')
+
+            #Remove @ symbols from xml attributes. There are no element/attribute collisions in the nmap xml (that we know of) so this should be OK.
+            [Regex]$MatchConvertedAmpersand = '(?m)(?<=\s+\")(@)(?=.+\"\:)'
+            $convertedJson = $convertedJson -replace $MatchConvertedAmpersand,''
+            $convertedJson
         }
-        'HashTable' {
-            #TODO: PSCore Method, add as potential feature flag
-            #$jsonResult | ConvertFrom-Json -AsHashtable
-            $nmapHashTable = $jsonResult | ConvertFrom-Json | ConvertPSObjectToHashtable
-            return $nmapHashTable
+
+        switch ($OutFormat) {
+            'JSON' {
+                return $jsonResult
+            }
+            'PSObject' {
+                return $jsonResult | ConvertFrom-Json
+            }
+            'HashTable' {
+                #TODO: PSCore Method, add as potential feature flag
+                #$jsonResult | ConvertFrom-Json -AsHashtable
+                $nmapHashTable = $jsonResult | ConvertFrom-Json | ConvertPSObjectToHashtable
+                return $nmapHashTable
+            }
         }
     }
 }
