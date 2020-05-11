f207dde65a91141234c1dc31e0bf548188c579a5
Justin Grote <jgrote@allieddigital.net>
2020-05-11 16:50:39 -0700

    PipelineTest

--- PSModule.build.ps1
+++ PSModule.build.ps1
@@ -1,6 +1,6 @@
 #requires -version 5.1
 
 #region PowerCDBootstrap
-. ([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://gist.githubusercontent.com/JustinGrote/2d3fdbac302847be33de8021add524ad/raw/76ab7c07e1c94395e3b7b256a274bd6d58bca19c/PowerCDBootstrap.ps1')))
+. ([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://gist.githubusercontent.com/JustinGrote/2d3fdbac302847be33de8021add524ad/raw/c940e074afbb924817680b91c3ec1120b5293117/PowerCDBootstrap.ps1')))
 
 #endregion PowerCDBootstrap
