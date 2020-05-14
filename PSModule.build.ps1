b9a57a83e69bc23eb28ff167d1a70cb1e21e8288
Justin Grote <jgrote@allieddigital.net>
2020-05-13 17:32:48 -0700

    PipelineTest

--- PSModule.build.ps1
+++ PSModule.build.ps1
@@ -4,7 +4,7 @@
 . ([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://git.io/PCDBootstrap')))
 #endregion PowerCDBootstrap
 
-task PoshNmap.TestPrereqs -Before PowerCD.Test {
+task TestPrereqs -Before PowerCD.Test.Pester {
     if (-not (command 'nmap' -ErrorAction SilentlyContinue)) {
         if ($isLinux) {
             apt-get install nmap
