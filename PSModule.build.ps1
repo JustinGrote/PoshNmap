11c3d24efe97ee287c67d1be737d851ecfdec39b
Justin Grote <jgrote@allieddigital.net>
2020-05-14 17:27:50 -0700

    PowerCD Version Bump and fix Invoke-Nmap Test

--- PSModule.build.ps1
+++ PSModule.build.ps1
@@ -1,15 +1,5 @@
 #requires -version 5.1
 
 #region PowerCDBootstrap
-. ([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://git.io/PCDBootstrap'))) -PowerCDVersion 0.8.2
+. ([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://git.io/PCDBootstrap'))) -PowerCDVersion 0.8.3
 #endregion PowerCDBootstrap
-
-task TestPrereqs -Before PowerCD.Test.Pester {
-    if (-not (command 'nmap' -ErrorAction SilentlyContinue)) {
-        if ($isLinux) {
-            sudo apt install nmap -y
-        } else {
-            choco install nmap -y
-        }
-    }
-}
--- Tests/Invoke-Nmap.tests.ps1
+++ Tests/Invoke-Nmap.tests.ps1
@@ -2,42 +2,56 @@ param (
     #Specify an alternate location for the Powershell Module. This is useful when testing a build in another directory
     [string]$ModulePath = (Get-Location)
 )
-
-#if we are in the "Tests" directory and there is a PSD file below this one, change to the module directory so relative paths work correctly.
-$currentDir = Get-Location
-if (
-    (Test-Path $currentDir -PathType Container) -and
-    $currentDir -match 'Tests$' -and
-    (Get-Item (join-path ".." "*.psd1") | where name -notmatch '\.(depend|requirements)\.psd1$')
-) {
-    $ModulePath = (split-path $modulepath)
+BeforeAll {
+    #If we are in invoke-build, use the built module, otherwise use the source module
+    if ($BuildRoot -and (Test-Path "$PSScriptRoot/../BuildOutput/PoshNmap/PoshNmap.psd1")) {
+        $moduleManifestFile = Resolve-Path ("$PSScriptRoot/../BuildOutput/PoshNmap/PoshNmap.psd1")
+    } else {
+        $moduleManifestFile = "$PSScriptRoot/../PoshNmap/PoshNmap.psd1"
+    }
+    Import-Module $moduleManifestFile
 }
 
-#If an alternate module root was specified, set that to our running directory.
-if ($ModulePath -ne (get-location).path) {Push-Location $ModulePath}
+# #if we are in the "Tests" directory and there is a PSD file below this one, change to the module directory so relative paths work correctly.
+# $currentDir = Get-Location
+# if (
+#     (Test-Path $currentDir -PathType Container) -and
+#     $currentDir -match 'Tests$' -and
+#     (Get-Item (join-path ".." "*.psd1") | where name -notmatch '\.(depend|requirements)\.psd1$')
+# ) {
+#     $ModulePath = (split-path $modulepath)
+# }
 
-#Find the module manifest. Get-ChildItem's last item is the deepest one available, so it will favor release builds over the raw source, but will use the source module if a release build is unavailable. #FIXME: Do this in a safer manner
-try {
-    $moduleManifestFile = Get-ChildItem -File -Recurse *.psd1 -ErrorAction Stop | where name -notmatch '(depend|requirements)\.psd1$'| Select-Object -last 1
-    $SCRIPT:moduleDirectory = $moduleManifestFile.directory
-} catch {
-    throw "Did not detect any module manifests in $ModulePath. Did you run 'Invoke-Build Build' first?"
-}
+# #If an alternate module root was specified, set that to our running directory.
+# if ($ModulePath -ne (get-location).path) {Push-Location $ModulePath}
 
-import-module $moduleManifestFile
+# #Find the module manifest. Get-ChildItem's last item is the deepest one available, so it will favor release builds over the raw source, but will use the source module if a release build is unavailable. #FIXME: Do this in a safer manner
+# try {
+#     $moduleManifestFile = Get-ChildItem -File -Recurse *.psd1 -ErrorAction Stop | where name -notmatch '(depend|requirements)\.psd1$'| Select-Object -last 1
+#     $SCRIPT:moduleDirectory = $moduleManifestFile.directory
+# } catch {
+#     throw "Did not detect any module manifests in $ModulePath. Did you run 'Invoke-Build Build' first?"
+# }
+
+# import-module $moduleManifestFile
 
 Describe "Invoke-Nmap" {
     $SCRIPT:nmapResult = "Test"
     #TODO: Figure out a better way to do this than a global variable, maybe get-variable -scope "up one"
     $GLOBAL:NmapPesterTestMockDir = (join-path $PSScriptRoot "Mocks")
-    Mock -Modulename PoshNmap InvokeNmapExe {
-        Get-Content "$NmapPesterTestMockDir\asusrouter.nmapxml"
-    }
-    Mock -Modulename PoshNmap InvokeNmapExe -parameterFilter {$argumentlist -match 'snmp-brute'} {
-        Get-Content "$NmapPesterTestMockDir\snmpresult.nmapxml"
+
+
+    BeforeAll {
+        Mock -Module PoshNmap InvokeNmapExe {
+            Get-Content "$NmapPesterTestMockDir\asusrouter.nmapxml"
+        }
+        Mock -Module PoshNmap InvokeNmapExe -parameterFilter {$argumentlist -match 'snmp-brute'} {
+            Get-Content "$NmapPesterTestMockDir\snmpresult.nmapxml"
+        }
     }
 
     It "Output: PSCustomObject by default" {
+
         $SCRIPT:nmapResult = Invoke-Nmap
         $nmapResult | Should -BeOfType [PSCustomObject]
     }
