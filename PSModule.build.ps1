bcd8a33174246dfec07c4538284fea4c06d94a60
Justin Grote <jgrote@allieddigital.net>
2020-05-20 12:03:06 -0700

    CI: Bump PowerCD to 0.8.5

--- PSModule.build.ps1
+++ PSModule.build.ps1
@@ -1,5 +1,5 @@
 #requires -version 5.1
 
 #region PowerCDBootstrap
-. ([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://git.io/PCDBootstrap'))) -PowerCDVersion 0.8.3
+. ([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://git.io/PCDBootstrap'))) -PowerCDVersion 0.8.5
 #endregion PowerCDBootstrap
