26c2889fa615ff89adc5799ba6c2893ecab6de64
Justin Grote <JustinGrote@users.noreply.github.com>
2021-11-08 12:03:55 -0800

    Update Tests for Pester 5

--- Tests/ConvertFrom-NmapXml.tests.ps1
+++ Tests/ConvertFrom-NmapXml.tests.ps1
@@ -1,61 +1,37 @@
-param (
-    #Specify an alternate location for the Powershell Module. This is useful when testing a build in another directory
-    [string]$ModulePath = (Get-Location)
-)
+Describe 'ConvertFrom-NmapXml' {
+    BeforeAll {
+        Import-Module (Resolve-Path $PSScriptRoot/../PoshNmap/PoshNmap.psd1) -Force
+        $Mocks = Join-Path $PSScriptRoot 'Mocks'
+        $asusNmapXmlFile = Join-Path $Mocks 'asusrouter.nmapxml'
+        $SCRIPT:asusNmapXmlContent = Get-Content $asusNmapXmlFile
+    }
 
-#if we are in the "Tests" directory, move up a directory
-$currentDir = Get-Location
-if (
-    (Test-Path $currentDir -PathType Container) -and
-    $currentDir -match 'Tests$'
-) {
-    $ModulePath = (split-path $modulepath)
-}
-
-#If an alternate module root was specified, set that to our running directory.
-if ($ModulePath -ne (get-location).path) {Push-Location $ModulePath}
-
-#Find the module manifest. Get-ChildItem's last item is the deepest one available, so it will favor release builds over the raw source, but will use the source module if a release build is unavailable. #FIXME: Do this in a safer manner
-try {
-    $moduleManifestFile = Get-ChildItem -File -Recurse *.psd1 -ErrorAction Stop | where name -notmatch '(depend|requirements)\.psd1$'| Select-Object -last 1
-    $SCRIPT:moduleDirectory = $moduleManifestFile.directory
-} catch {
-    throw "Did not detect any module manifests in $ModulePath. Did you run 'Invoke-Build Build' first?"
-}
-
-import-module $moduleManifestFile
-
-Describe "ConvertFrom-NmapXml" {
-    $Mocks = join-path $PSScriptRoot "Mocks"
-    $asusNmapXmlFile = join-path $Mocks "asusrouter.nmapxml"
-    $asusNmapXmlContent = get-content $asusNmapXmlFile
-
-    It "Input: Get-Content from file (string array)" {
+    It 'Input: Get-Content from file (string array)' {
         $asusNmapXmlContent | ConvertFrom-NmapXml | Should -BeOfType [PSCustomObject]
     }
-    It -Pending "Input: Single Unindented string (xml output maybe)" {
-        [String]($asusNmapXmlContent) | ConvertFrom-NmapXml | Should -BeOfType [PSCustomObject]
-    }
-    It "Input: XML" {
+    It 'Input: XML' {
         [XML]($asusNmapXmlContent) | ConvertFrom-NmapXml
     }
-    It "Output: PoshNmap by Default" {
+    It 'Output: PoshNmap by Default' {
         $asusNmapXmlContent | ConvertFrom-NmapXml | Should -BeOfType [PSCustomObject]
     }
-    It "Output: PSObject with -OutFormat PSObject" {
+    It -Pending 'Input: Single Unindented string (xml output maybe)' {
+        [String]($asusNmapXmlContent) | ConvertFrom-NmapXml | Should -BeOfType [PSCustomObject]
+    }
+    It 'Output: PSObject with -OutFormat PSObject' {
         $asusNmapXmlContent | ConvertFrom-NmapXml -OutFormat PSObject | Should -BeOfType [PSCustomObject]
     }
-    It "Output: HashTable with -OutFormat HashTable" {
+    It 'Output: HashTable with -OutFormat HashTable' {
         $asusNmapXmlContent | ConvertFrom-NmapXml -OutFormat HashTable | Should -BeOfType [HashTable]
     }
-    It "Output: NmapResult with -OutFormat PoshNmap" {
-        $nmapResult = $asusNmapXmlContent | ConvertFrom-NmapXml -OutFormat PoshNmap
-        $nmapResult | Should -Not -BeNullOrEmpty
-        $nmapResult | ForEach-Object {
-            'PoshNmapHost' | Should -BeIn $PSItem.psobject.typenames
-        }
-    }
-    It "Output: NmapSummary with -OutFormat Summary" {
+    # It 'Output: NmapResult with -OutFormat PoshNmap' {
+    #     $nmapResult = $asusNmapXmlContent | ConvertFrom-NmapXml -OutFormat PoshNmap
+    #     $nmapResult | Should -Not -BeNullOrEmpty
+    #     $nmapResult | ForEach-Object {
+    #         'PoshNmapHost' | Should -BeIn $PSItem.psobject.typenames
+    #     }
+    # }
+    It 'Output: NmapSummary with -OutFormat Summary' {
         $nmapSummary = $asusNmapXmlContent | ConvertFrom-NmapXml -OutFormat Summary
         $nmapSummary | Should -Not -BeNullOrEmpty
         $nmapSummary | ForEach-Object {
--- Tests/Invoke-Nmap.tests.ps1
+++ Tests/Invoke-Nmap.tests.ps1
@@ -1,71 +1,77 @@
-param (
-    #Specify an alternate location for the Powershell Module. This is useful when testing a build in another directory
-    [string]$ModulePath = (Get-Location)
-)
-BeforeAll {
-    #If we are in invoke-build, use the built module, otherwise use the source module
-    if ($BuildRoot -and (Test-Path "$PSScriptRoot/../BuildOutput/PoshNmap/PoshNmap.psd1")) {
-        $moduleManifestFile = Resolve-Path ("$PSScriptRoot/../BuildOutput/PoshNmap/PoshNmap.psd1")
-    } else {
-        $moduleManifestFile = "$PSScriptRoot/../PoshNmap/PoshNmap.psd1"
-    }
-    Import-Module $moduleManifestFile
-}
-
-# #if we are in the "Tests" directory and there is a PSD file below this one, change to the module directory so relative paths work correctly.
-# $currentDir = Get-Location
-# if (
-#     (Test-Path $currentDir -PathType Container) -and
-#     $currentDir -match 'Tests$' -and
-#     (Get-Item (join-path ".." "*.psd1") | where name -notmatch '\.(depend|requirements)\.psd1$')
-# ) {
-#     $ModulePath = (split-path $modulepath)
-# }
-
-# #If an alternate module root was specified, set that to our running directory.
-# if ($ModulePath -ne (get-location).path) {Push-Location $ModulePath}
-
-# #Find the module manifest. Get-ChildItem's last item is the deepest one available, so it will favor release builds over the raw source, but will use the source module if a release build is unavailable. #FIXME: Do this in a safer manner
-# try {
-#     $moduleManifestFile = Get-ChildItem -File -Recurse *.psd1 -ErrorAction Stop | where name -notmatch '(depend|requirements)\.psd1$'| Select-Object -last 1
-#     $SCRIPT:moduleDirectory = $moduleManifestFile.directory
-# } catch {
-#     throw "Did not detect any module manifests in $ModulePath. Did you run 'Invoke-Build Build' first?"
-# }
-
-# import-module $moduleManifestFile
-
-Describe "Invoke-Nmap" {
-    $SCRIPT:nmapResult = "Test"
-    #TODO: Figure out a better way to do this than a global variable, maybe get-variable -scope "up one"
-    $GLOBAL:NmapPesterTestMockDir = (join-path $PSScriptRoot "Mocks")
 
 
-    BeforeAll {
-        Mock -Module PoshNmap InvokeNmapExe {
-            Get-Content "$NmapPesterTestMockDir\asusrouter.nmapxml"
+# # #if we are in the "Tests" directory and there is a PSD file below this one, change to the module directory so relative paths work correctly.
+# # $currentDir = Get-Location
+# # if (
+# #     (Test-Path $currentDir -PathType Container) -and
+# #     $currentDir -match 'Tests$' -and
+# #     (Get-Item (join-path ".." "*.psd1") | where name -notmatch '\.(depend|requirements)\.psd1$')
+# # ) {
+# #     $ModulePath = (split-path $modulepath)
+# # }
+
+# # #If an alternate module root was specified, set that to our running directory.
+# # if ($ModulePath -ne (get-location).path) {Push-Location $ModulePath}
+
+# # #Find the module manifest. Get-ChildItem's last item is the deepest one available, so it will favor release builds over the raw source, but will use the source module if a release build is unavailable. #FIXME: Do this in a safer manner
+# # try {
+# #     $moduleManifestFile = Get-ChildItem -File -Recurse *.psd1 -ErrorAction Stop | where name -notmatch '(depend|requirements)\.psd1$'| Select-Object -last 1
+# #     $SCRIPT:moduleDirectory = $moduleManifestFile.directory
+# # } catch {
+# #     throw "Did not detect any module manifests in $ModulePath. Did you run 'Invoke-Build Build' first?"
+# # }
+
+# # import-module $moduleManifestFile
+
+Describe 'Invoke-Nmap' {
+    Context 'ASUS Router Mock' {
+        BeforeAll {
+            [string]$ModulePath = (Get-Location)
+            #If we are in invoke-build, use the built module, otherwise use the source module
+            if ($BuildRoot -and (Test-Path "$PSScriptRoot/../BuildOutput/PoshNmap/PoshNmap.psd1")) {
+                $moduleManifestFile = Resolve-Path ("$PSScriptRoot/../BuildOutput/PoshNmap/PoshNmap.psd1")
+            } else {
+                $moduleManifestFile = "$PSScriptRoot/../PoshNmap/PoshNmap.psd1"
+            }
+            Import-Module $moduleManifestFile
+
+            $SCRIPT:nmapResult = 'Test'
+            #TODO: Figure out a better way to do this than a global variable, maybe get-variable -scope "up one"
+            $GLOBAL:NmapPesterTestMockDir = (Join-Path $PSScriptRoot 'Mocks')
+
+            Mock -Module PoshNmap InvokeNmapExe {
+                Get-Content "$NmapPesterTestMockDir\asusrouter.nmapxml"
+            }
+            Mock -Module PoshNmap InvokeNmapExe -ParameterFilter { $argumentlist -match 'snmp-brute' } {
+                Get-Content "$NmapPesterTestMockDir\snmpresult.nmapxml"
+            }
         }
-        Mock -Module PoshNmap InvokeNmapExe -parameterFilter {$argumentlist -match 'snmp-brute'} {
-            Get-Content "$NmapPesterTestMockDir\snmpresult.nmapxml"
+
+        It 'Output: PSCustomObject by default' {
+            $SCRIPT:nmapResult = Invoke-Nmap
+            $nmapResult | Should -BeOfType [PSCustomObject]
+        }
+
+        It 'Output: PoshNmap Output Data Sanity Check' {
+            (Invoke-Nmap).nmapresult.ports.port | Where-Object portid -Match '445' | ForEach-Object protocol | Should -Be 'tcp'
+        }
+
+        It 'Output: XML Data Sanity Check' {
+            (Invoke-Nmap -OutFormat PSObject).host.ports.port | Where-Object portid -Match '445' | ForEach-Object protocol | Should -Be 'tcp'
+        }
+
+        #Fixme: Mock SNMP
+        It 'Output: SNMP Table output is correct' -Pending {
+            (Invoke-Nmap -Snmp).ports.scriptresult.'snmp-brute'.table | Where-Object password -Match 'public' | Should -Not -BeNullOrEmpty
         }
     }
 
-    It "Output: PSCustomObject by default" {
-
-        $SCRIPT:nmapResult = Invoke-Nmap
-        $nmapResult | Should -BeOfType [PSCustomObject]
+    Describe 'Live Tests' {
+        It 'Test Google' {
+            $nmapResult = Invoke-Nmap -computerName 'www.google.com'
+        }
     }
 
-    It "Output: PoshNmap Output Data Sanity Check" {
-        (Invoke-Nmap).nmapresult.ports.port | where portid -match '445' | % protocol | should -be 'tcp'
-    }
 
-    It "Output: XML Data Sanity Check" {
-        (Invoke-Nmap -OutFormat PSObject).host.ports.port | where portid -match '445' | % protocol | should -be 'tcp'
-    }
 
-    #Fixme: Mock SNMP
-    It "Output: SNMP Table output is correct" -Pending {
-        (Invoke-Nmap -snmp).ports.scriptresult.'snmp-brute'.table | where password -match 'public' | Should -Not -BeNullOrEmpty
-    }
 }
